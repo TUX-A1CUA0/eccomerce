f2632f16ba1ccd9a11b69750ba154d6ff8e3af92
TUX-A1CUA0 <Un1x.Cuassant@gmail.com>
2024-11-04 02:20:27 +0800

    c
    change

--- FRONTEND/src/app/pages/contact/contact.component.ts
+++ FRONTEND/src/app/pages/contact/contact.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from '../../components/header/header.component';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
