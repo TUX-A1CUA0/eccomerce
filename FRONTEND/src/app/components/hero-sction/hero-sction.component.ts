172d718736362688dc1b3076b831c954c4338d7d
TUX-A1CUA0 <Un1x.Cuassant@gmail.com>
2024-11-04 18:03:46 +0800

    changes
    changes

--- FRONTEND/src/app/components/hero-sction/hero-sction.component.ts
+++ FRONTEND/src/app/components/hero-sction/hero-sction.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  NgZone,
+  ChangeDetectorRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-hero-sction',
   standalone: true,
   imports: [],
   templateUrl: './hero-sction.component.html',
-  styleUrl: './hero-sction.component.css'
+  styleUrls: ['./hero-sction.component.css'],
 })
 export class HeroSctionComponent {
+  x = 0;
+  y = 0;
 
+  constructor(private ngZone: NgZone, private cdr: ChangeDetectorRef) {}
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    this.ngZone.run(() => {
+      this.x = e.clientX;
+      this.y = e.clientY;
+      this.cdr.detectChanges();
+    });
+  }
+
+  // Add this method to force change detection when needed
+  detectChanges() {
+    this.cdr.detectChanges();
+  }
 }
